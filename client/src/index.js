7c3f2fffd6e4c024eedb365eddf6e3447079261f
yuriy goncharuk <goncharukyou@gmail.com>
2020-01-30 15:36:16 +0200

    hour

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ function component() {
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
     return element;
 }
 
---
+++ client/src/scripts/observer.js
@@ -0,0 +1,20 @@
+export class Observable {
+    constructor() {
+        this.observers = [];
+    }
+
+    addObserver(obj) {
+        this.observers.push(obj);
+    }
+
+    delObserver(obj) {
+        this.observers.splice(this.observers.indexOf(obj));
+    }
+
+    notifyAll(e, data) {
+        console.log(e);
+        for (var i = 0; i < this.observers.length; i++) {
+            this.observers[i].on(this, e, data);
+        }
+    }
+}
